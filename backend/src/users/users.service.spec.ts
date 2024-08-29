134890c3269e707ccbe3793f3ac9065e1dbd1b0b
johan-bouguermouh <91847583+johan-bouguermouh@users.noreply.github.com>
2024-08-29 11:26:23 +0200

    feat: add units tests userService

--- backend/src/users/users.service.spec.ts
+++ backend/src/users/users.service.spec.ts
@@ -1,18 +1,179 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository, UpdateResult } from 'typeorm';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let userRepository: Repository<User>;
+  const USER_REPOSITORY_TOKEN = getRepositoryToken(User);
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: USER_REPOSITORY_TOKEN,
+          useValue: {
+            save: jest.fn((x) => new User(x)),
+            find: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    userRepository = module.get<Repository<User>>(USER_REPOSITORY_TOKEN);
   });
 
-  it('should be defined', () => {
+  it('UserService should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('UserRepository should be defined', () => {
+    expect(userRepository).toBeDefined();
+  });
+
+  describe('Methode create :', () => {
+    /** ARRANGE */
+    const createUserMock: User = new User({
+      firstName: 'Johan',
+      lastName: 'Bouguermouh',
+      email: 'johan.bouguermouh@laplateforme.io',
+      password: 'Test_1234!',
+    });
+
+    it('Password should be hashed', async () => {
+      /** ARRANGE */
+      const hashPassword = jest
+        .spyOn(createUserMock, 'hashPassword')
+        .mockResolvedValue();
+
+      /** ACT **/
+      await service.create(createUserMock);
+
+      /** ASSERT **/
+      expect(hashPassword).toHaveBeenCalled();
+    });
+
+    it('Should retrun User by create', async () => {
+      /** ARRANGE */
+      jest.spyOn(userRepository, 'save').mockResolvedValue(createUserMock);
+
+      /** ACT **/
+      const callCreateMethodService = async () => {
+        return await service.create(createUserMock);
+      };
+
+      /** ASSERT **/
+      expect(await callCreateMethodService()).toEqual(createUserMock);
+    });
+  });
+
+  describe('Methode findAll :', () => {
+    it('Should return an array of users', async () => {
+      /** ARRANGE */
+      const getAllUsersMock = [
+        new User({
+          firstName: 'jhon',
+          lastName: 'doe',
+          email: 'jhondoe@mail.fr',
+          password: '*BA836B173CB7FD26F1683B52974AF4310BCAFC00',
+        }),
+        new User({
+          firstName: 'jane',
+          lastName: 'doe',
+          email: 'janedoe@mail.fr',
+          password: '*BA836B173CB7FD26F1683B52974AF4310BCAFC00',
+        }),
+      ];
+      jest.spyOn(userRepository, 'find').mockResolvedValue(getAllUsersMock);
+
+      /** ACT **/
+      const callFindAllMethodService = async () => {
+        return await service.findAll();
+      };
+
+      /** ASSERT **/
+      expect(await callFindAllMethodService()).toEqual(getAllUsersMock);
+    });
+
+    it('Should return an empty array', async () => {
+      /** ARRANGE */
+      jest.spyOn(userRepository, 'find').mockResolvedValue([]);
+
+      /** ACT **/
+      const callFindAllMethodService = async () => {
+        return await service.findAll();
+      };
+
+      /** ASSERT **/
+      expect(await callFindAllMethodService()).toEqual([]);
+    });
+  });
+
+  describe('Methode findOne :', () => {
+    it('Should return a user', async () => {
+      /** ARRANGE */
+      const getOneUserMock = new User({
+        firstName: 'jhon',
+        lastName: 'doe',
+        email: 'jhondoe@mail.fr',
+        password: '*BA836B173CB7FD26F1683B52974AF4310BCAFC00',
+      });
+
+      jest.spyOn(userRepository, 'findOne').mockResolvedValue(getOneUserMock);
+
+      /** ACT **/
+      const callFindOneMethodService = async () => {
+        return await service.findOne(1);
+      };
+
+      /** ASSERT **/
+      expect(await callFindOneMethodService()).toEqual(getOneUserMock);
+    });
+
+    it('Should return null', async () => {
+      /** ARRANGE */
+      jest.spyOn(userRepository, 'findOne').mockResolvedValue(null);
+
+      /** ACT **/
+      const callFindOneMethodService = async () => {
+        return await service.findOne(1);
+      };
+
+      /** ASSERT **/
+      expect(await callFindOneMethodService()).toBeNull();
+    });
+  });
+
+  describe('Methode update :', () => {
+    it('Should return an object', async () => {
+      /** ARRANGE */
+      const updateUserMock = new UpdateUserDto({
+        email: 'jhondoeNew@mail.fr',
+      });
+
+      const updateResultMock: UpdateResult = {
+        generatedMaps: [],
+        raw: [],
+        affected: 1,
+      };
+
+      jest.spyOn(userRepository, 'update').mockResolvedValue(updateResultMock);
+
+      /** ACT **/
+      const callUpdateMethodService = async () => {
+        return await service.update(1, updateUserMock);
+      };
+
+      /** ASSERT **/
+      expect(await callUpdateMethodService()).toEqual(updateResultMock);
+    });
   });
 });
