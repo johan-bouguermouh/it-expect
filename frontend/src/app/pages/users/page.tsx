3c03cec336318be0b7e2d3c936368325d7111d5a
johan-bouguermouh <91847583+johan-bouguermouh@users.noreply.github.com>
2024-09-01 13:31:09 +0200

    feat: update index pages and add users pages

---
+++ frontend/src/app/pages/users/page.tsx
@@ -0,0 +1,33 @@
+// pages/posts.jsx
+import { getUsers } from "@/services/userService";
+import { dehydrate, QueryClient, useQuery } from "@tanstack/react-query";
+
+// Création de l'interfce utilisateur
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  password: string;
+}
+
+function Users() {
+  // This useQuery could just as well happen in some deeper child to
+  // the "Posts"-page, data will be available immediately either way
+  const { data: users } = useQuery({
+    queryKey: ["users"],
+    queryFn: () => getUsers(),
+  });
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {users?.map((user: any) => (
+          <li key={user.id}>{user.firstName + " " + user.lastName}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Users;
